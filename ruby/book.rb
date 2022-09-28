b40d86fe10a45b5bca76348291e99f36e8093452
Tushar <singhtushar348@gmail.com>
2022-09-28 18:27:47 +0100

    Create has many rentals relation

--- ruby/book.rb
+++ ruby/book.rb
@@ -1,8 +1,14 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rental
 
   def initialize(title, author)
     @author = author
     @title = title
+    @rental = []
+  end
+
+  def add_rentals(rental)
+    @rental.push(rental)
+    rental.book = self
   end
 end
