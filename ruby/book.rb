39bec53c8f43be23d02a78ee1463c506e663f6dc
Tushar <singhtushar348@gmail.com>
2022-09-28 19:20:43 +0100

    Implement many to many relation b/w Person and Book

--- ruby/book.rb
+++ ruby/book.rb
@@ -6,9 +6,4 @@ class Book
     @title = title
     @rental = []
   end
-
-  def add_rentals(rental)
-    @rental.push(rental)
-    rental.book = self
-  end
 end
--- ruby/person.rb
+++ ruby/person.rb
@@ -2,13 +2,14 @@ require_relative './nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rental
 
   def initialize(age, name = 'unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @id = Random.rand(1..1000)
+    @rental = []
     super()
   end
 
