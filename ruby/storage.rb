95885cab83347af75abe86317813bd929cf51c73
Tushar <singhtushar348@gmail.com>
2022-10-04 20:25:29 +0530

    Modify storage to append data

--- ruby/storage.rb
+++ ruby/storage.rb
@@ -2,25 +2,55 @@ require 'json'
 
 class Storage
   def store_books(books)
-    file = File.open('./data/books.json', 'a') do |f|
-      books.each do |book|
-        f << (JSON.pretty_generate(['title': book.title,'author': book.author]))
+    books.each do |book|
+      if(File.size?('./data/books.json'))
+        file = File.read('./data/books.json')
+        books_file = JSON.parse(file)
+        books_file << {title: book.title, author: book.author}
+
+        File.open('./data/books.json', 'w') do |f|
+        f.write(JSON.pretty_generate(books_file))
+        end
+      else
+        File.open('./data/books.json', 'w') do |f|
+          f.write(JSON.pretty_generate([{title: book.title, author: book.author}]))
+        end
       end
     end
   end
 
   def store_people(people)
-    file = File.open('./data/people.json', 'a') do |f|
-      people.each do |people|
-      f.write(JSON.pretty_generate(['type': people.class,'Name': people.name, 'Age': people.age]))
+    people.each do |person|
+      if(File.size?('./data/people.json'))
+        file = File.read('./data/people.json')
+        people_file = JSON.parse(file)
+        people_file << {type: person.class, Name: person.name, Age: person.age}
+
+        File.open('./data/people.json', 'w') do |f|
+        f.write(JSON.pretty_generate(people_file))
+        end
+      else
+        File.open('./data/people.json', 'w') do |f|
+          f.write(JSON.pretty_generate([{type: person.class, Name: person.name, Age: person.age}]))
+        end
       end
     end
   end
 
   def store_rentals(rentals)
-    file = File.open('./data/rentals.json', 'a') do |f|
-      rentals.each do |rental|
-      f.write(JSON.pretty_generate(['Date': rental.date,'book': rental.book.title, 'Person': rental.person.name]))
+    rentals.each do |rental|
+      if(File.size?('./data/rentals.json'))
+        file = File.read('./data/rentals.json')
+        rentals_file = JSON.parse(file)
+        rentals_file << { Date: rental.date, book: rental.book.title, Person: rental.person.name }
+
+        File.open('./data/rentals.json', 'w') do |f|
+        f.write(JSON.pretty_generate(rentals_file))
+        end
+      else
+        File.open('./data/rentals.json', 'w') do |f|
+          f.write(JSON.pretty_generate([{ Date: rental.date, book: rental.book.title, Person: rental.person.name }]))
+        end
       end
     end
   end
