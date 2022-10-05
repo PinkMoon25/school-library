96787ecedd3191b188a13acc7f367e694dad56e7
Tushar <singhtushar348@gmail.com>
2022-10-05 15:47:14 +0530

    Include method to dave data from the app

--- ruby/app.rb
+++ ruby/app.rb
@@ -6,6 +6,7 @@ require_relative 'teacher'
 require_relative 'nameable'
 require_relative 'classroom'
 require_relative 'decorator'
+require_relative 'storage'
 require 'json'
 
 class App
@@ -15,6 +16,8 @@ class App
     @books = []
     @people = []
     @rentals = []
+    Storage.load_books(books)
+    Storage.load_people(people)
   end
 
   def create_book
@@ -22,9 +25,16 @@ class App
     title = gets.chomp
     print 'Author: '
     author = gets.chomp
-    @books.push(Book.new(title, author)) if title && author
+    @books.push(Book.new(title, author))
+    if File.size?('./data/books.json')
+      file = File.read('./data/books.json')
+      books_file = JSON.parse(file)
+      books_file << { title: title, author: author }
+      File.write('./data/books.json', JSON.pretty_generate(books_file))
+      else
+        File.write('./data/books.json', JSON.pretty_generate([{ title: title, author: author }]))
+      end
     puts 'Book created successfully'
-    p @books
   end
 
   def create_student
@@ -38,6 +48,15 @@ class App
     permission = gets.chomp
     parent_permission = !(permission.downcase == 'n' || permission.downcase == 'no')
     @people.push(Student.new(age, classroom, name, parent_permission: parent_permission))
+    if File.size?('./data/people.json')
+      file = File.read('./data/people.json')
+      student_file = JSON.parse(file)
+      student_file << { type:'Student', Classroom: classroom , Name: name, Age: age}
+      File.write('./data/people.json', JSON.pretty_generate(student_file))
+    else
+      File.write('./data/people.json',
+                 JSON.pretty_generate([{ type:'Student', Classroom: classroom , Name: name, Age: age}]))
+    end
     puts 'Student created successfully'
   end
 
@@ -49,6 +68,15 @@ class App
     print 'Name: '
     name = gets.chomp
     @people.push(Teacher.new(age, specialization, name))
+    if File.size?('./data/people.json')
+      file = File.read('./data/people.json')
+      teacher_file = JSON.parse(file)
+      teacher_file << { type:'Teacher', specialization: specialization , Name: name, Age: age}
+      File.write('./data/people.json', JSON.pretty_generate(teacher_file))
+    else
+      File.write('./data/people.json',
+                 JSON.pretty_generate([{ type:'Teacher', specialization: specialization , Name: name, Age: age}]))
+    end
     puts 'Teacher created successfully'
   end
 
@@ -66,23 +94,11 @@ class App
   end
 
   def list_books
-    if File.exist?('./data/books.json')
-      file = File.read('./data/books.json')
-      data_hash = JSON.parse(file)
-      data_hash.each do |book|
-        puts "Title: #{book['title']}, Author: #{book['author']}"
-      end
-    end
+    @books.each_with_index { |book, idx| puts "#{idx}) Title: #{book.title}, Author: #{book.author}" }
   end
 
   def list_people
-    if File.exist?('./data/people.json')
-      file = File.read('./data/people.json')
-      data_hash = JSON.parse(file)
-      data_hash.each do |person|
-        puts "ID: #{person['id']} Type: #{person['type']}, Name: #{person['Name']}, Age: #{person['Age']}"
-      end
-    end
+    @people.each_with_index { |p, idx| puts "#{idx}) [#{p.class}] Name: #{p.name}, Age: #{p.age}, Id: #{p.id}" }
   end
 
   def create_rental
@@ -97,14 +113,26 @@ class App
     print 'Date: '
     date = gets.chomp
     @rentals.push(Rental.new(date, book, person)) if book && person
+    if File.size?('./data/rentals.json')
+      file = File.read('./data/rentals.json')
+      rentals_file = JSON.parse(file)
+      rentals_file << { Date: date, title: book.title, author: book.author, age: person.age, name:
+      person.name, id: person.id}
+
+      File.write('./data/rentals.json', JSON.pretty_generate(rentals_file))
+    else
+      File.write('./data/rentals.json', JSON.pretty_generate([{ Date: date, title: book.title, author: book.author, age: person.age, name:
+        person.name, id: person.id}]))
+    end
     puts 'Rental created successfully'
   end
 
   def list_rentals
     print 'Id of the Person: '
-    person_id = gets.chomp.to_i
-    select_rentals = @rentals.select { |i| i.person.id == person_id }
-    puts "No person with id: #{person_id}" if select_rentals.empty?
-    select_rentals.each { |r| puts "Date: #{r.date}, Book #{r.book.title} by #{r.book.author}" }
+    id = gets.chomp.to_i
+    rentals_file = Storage.load_rentals
+    select_rentals = rentals_file.select { |i| i['id'].to_i == id }
+    puts "No person with id: #{id} check rentals.json for the id" if select_rentals.empty?
+    select_rentals.each { |r| puts "Date: #{r['Date']}, Book #{r['title']} by #{r['author']}" }
   end
 end
